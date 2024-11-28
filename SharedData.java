2097eee7198f1eaf7ab0a217d20f1aea563295b0
HelalAli <helalali358@gmail.com>
2024-11-28 16:09:10 +0200

    msg

--- SharedData.java
+++ SharedData.java
@@ -11,7 +11,7 @@ public class SharedData
     /**
      * The array of integers shared between components.
      */
-    private ArrayList<Integer> TEAMMATE2;
+    private ArrayList<Integer> TEAMMATE3;
 
     /**
      * The array indicating win states for components.
